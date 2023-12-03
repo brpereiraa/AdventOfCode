37fa9b2951e328d4326078b2730bb3b5875cecac
Bruno <brunoomiguel10@hotmail.com>
2023-12-03 02:36:12 +0000

    Removing target

--- Day2/d2p1/target/debug/d2p1.d
+++
@@ -1,1 +0,0 @@
-/home/brunolopes/Documents/Code/AdventOfCode/Day2/d2p1/target/debug/d2p1: /home/brunolopes/Documents/Code/AdventOfCode/Day2/d2p1/src/main.rs
--- Day2/d2p1/target/debug/deps/d2p1-1ddf593f83409a93.d
+++
@@ -1,5 +0,0 @@
-/home/brunolopes/Documents/Code/AdventOfCode/Day2/d2p1/target/debug/deps/d2p1-1ddf593f83409a93: src/main.rs
-
-/home/brunolopes/Documents/Code/AdventOfCode/Day2/d2p1/target/debug/deps/d2p1-1ddf593f83409a93.d: src/main.rs
-
-src/main.rs:
